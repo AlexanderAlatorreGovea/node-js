cc2f98b8838d49503b8595b68760f1123391308f
Alexander <Alexander_alatorre1993@hotmail.com>
2022-10-05 17:27:42 -0600

    added race.js

--- race.js
+++ race.js
@@ -1,2 +1,11 @@
-console.log('🐰  finishes!')
-console.log('🐢 finishes!')
+// sync
+console.log("🐰  finishes!");
+console.log("🐢 finishes!");
+
+// async
+
+setTimeout(() => {
+  console.log("🐰 async finishes!");
+}, 200);
+
+console.log("🐢 sync finishes!");
