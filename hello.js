8e63eacf3051d9e23e8556dc02cc4409aa1d88bc
Alexander <Alexander_alatorre1993@hotmail.com>
2022-10-05 16:32:03 -0600

    added console logs

--- hello.js
+++ hello.js
@@ -1,7 +1,19 @@
-const mission = "learn";
+//const mission = "learn";
+// run node hello.js and you will get the same as the 
+// second console.log
+// run: node hello.js explore
+// const mission = process.argv[2]
 
-if (mission === "learn") {
-  console.log("time to write some node code");
-} else {
-  console.log(`Is ${mission} really fun`);
-}
+
+// if (mission === "learn") {
+//   console.log("time to write some node code");
+// } else {
+//   console.log(`Is ${mission} really fun`);
+// }
+
+//console.log(process.argv[2])
+console.log(module)
+//console.log(__filename)
+//console.log(__dirname)
+
+//console.log(global)
